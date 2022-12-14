5654ad8e02e390e8d1912b1e4dc8601ba812c352
Enzo Degraeve <enzodeg40@gmail.com>
2022-12-14 22:14:58 +0100

    Fix bug about process.exit

--- cli.js
+++ cli.js
@@ -11,14 +11,14 @@ if(args.length == 0) {
     console.log('db-commit remove   Remove all files in the database folder');
     console.log('db-commit push     Push all files in the database folder to the database');
     console.log('db-commit pull     Pull all files from the database to the database folder');
-    process.exit(1);
+    process.exit(0);
 }
 
 // If first argument is remove, remove all files in the database folder
 if(args[0] == 'remove') {
     const a = require('./remove');
     a();
-    process.exit(1);
+    process.exit(0);
 }
 
 // Print hello world provided arguments
@@ -83,3 +83,5 @@ sql.query('SHOW TABLES', function (error, results, fields) {
         }
     });
 });
+
+process.exit(0);
