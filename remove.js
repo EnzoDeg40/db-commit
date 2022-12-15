4d5d7ebf1850e20a989b7c626b32c09bc00307e1
Enzo Degraeve <enzodeg40@gmail.com>
2022-12-15 19:17:03 +0100

    Fix remove command

--- remove.js
+++ remove.js
@@ -5,16 +5,8 @@ function remove() {
     let currentDir = process.cwd();
     console.log(currentDir);
     
-    // Read the database folder
-    fs.readdir(currentDir, function(err, files) {
-        if(err) {
-            console.log(err);
-        }
-
-        console.log(files);
-    });
-
-    console.log('All files removed');
+    // Remove the database folder
+    fs.rmdirSync(currentDir + '/database', { recursive: true });
 }
 
 module.exports = remove;
